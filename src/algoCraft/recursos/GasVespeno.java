3748feb7b4e48724f3810f11288467c9d13bec95
Matias <Matias@Matiasnote-PC>
2015-06-25 18:53:51 -0300

    asd

--- src/algoCraft/recursos/GasVespeno.java
+++ src/algoCraft/recursos/GasVespeno.java
@@ -2,8 +2,10 @@ package recursos;
 
 public class GasVespeno extends Recurso {
 
-	public GasVespeno(int x, int y) {
+	public GasVespeno(int x, int y,int alto,int ancho) {
 		super(x, y);
+		this.alto=alto;
+		this.ancho=ancho;
 	}
 
 }
